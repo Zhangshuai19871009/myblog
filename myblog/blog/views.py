332d8260a852831392cb2882ef31f4d7cd27543c
sunny <270800559@qq.com>
2018-09-05 21:46:04 +0800

    使用css优化页面

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -6,6 +6,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     return render(request, 'blog/blog_list.html', context)
 
 # 博客详情页
@@ -18,6 +19,7 @@ def blog_detail(request, blog_pk):
 def blogs_with_type(request, blogs_with_type):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blogs_with_type)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_types'] = BlogType.objects.all()
     return render(request, 'blog/blogs_with_type.html', context)
